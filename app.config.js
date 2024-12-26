cab84abfe0d7dd7e9b59f914d02b767ec1b475ce
x0us <luli@vziro.com>
2024-12-26 09:04:25 +0800

    12/26

--- app.config.js
+++ app.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from '@solidjs/start/config'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  start: {
-    ssr: true,
-    server: {
-      baseURL: process.env.BASE_PATH,
-      preset: "static"
-    }
-  },
+  ssr: true,
+  server: {
+    baseURL: process.env.BASE_PATH,
+    preset: "static"
+  }
+  ,
   vite: {
     plugins: [UnoCSS()],
   },
